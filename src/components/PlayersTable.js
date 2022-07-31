714a0150f34383fb024f1488dedcfd6ed8fc38b5
Kamil Stawik <kamil_stawik@op.pl>
2022-07-31 15:43:59 +0200

    material DataTable added to project

--- src/components/PlayersTable.js
+++ src/components/PlayersTable.js
@@ -1,19 +1,51 @@
 import players from "../content/players";
 import matchHistory from "../content/matchHistory";
 import { getRanking } from "../content/ranking";
+import { DataGrid } from '@mui/x-data-grid';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 const PlayersTable = (props) => {
   const tableColumnsNumber = 2;
   const ranking = getRanking(players, matchHistory);
   console.log('Ranking:', ranking)
 
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 80 },
+    { field: 'firstName', headerName: 'Imię', width: 130 },
+    { field: 'playerWins', headerName: 'Zwycięstwa', width: 200 },
+  ];
+
 
   return (
     <div style={{ width: "1000px", margin: "auto" }}>
+      {console.log('DataGrid ranking:', ranking)}
 
-      <h2 style={{ width: "max-content", margin: "auto", marginTop: "50px"}}> {props.title}</h2>
+      <h2 style={{ width: "max-content", margin: "auto", marginTop: "50px" }}> {props.title}</h2>
 
-      <table style={{ width: "1000px" }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 600 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nick</TableCell>
+              <TableCell align="right">Zwycięstwa</TableCell>
+              <TableCell align="right">Wygrane Legi</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ranking.map((rankingPosition) => (
+              <TableRow
+                key={rankingPosition.firstName}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{rankingPosition.firstName}</TableCell>
+                <TableCell align="right">{rankingPosition.playerWins}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/* <table style={{ width: "1000px" }}>
         <tbody>
           <tr>
             <th style={{ width: `${1000 / tableColumnsNumber}px` }}> Nick Gracza</th>
@@ -26,7 +58,7 @@ const PlayersTable = (props) => {
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
     </div>
   );
 };
